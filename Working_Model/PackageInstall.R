0ad0c870e101612589d92a092d6745c92ce1d95d
Owen Peterson <peterowe@oregonstate.edu>
2025-06-10 19:31:15 -0700

    Fixed error in InstallPackages.R

--- Working_Model/PackageInstall.R
+++ Working_Model/PackageInstall.R
@@ -1,5 +1,6 @@
 #installing devtools to install bioracler from GitHub. 
 #Link to biooraclerr repo: https://github.com/bio-oracle/biooracler
+options(repos = c(CRAN = "https://cloud.r-project.org"))
 install.packages("devtools")
 library(devtools)
 devtools::install_github("bio-oracle/biooracler")
